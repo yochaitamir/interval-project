ca905f9bd43b006df252e21333fce07cd9dd199e
yochaitamir <yochai1911@gmail.com>
2018-09-28 14:31:38 +0300

    second commit

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
 
     $("#submit").click(function () {
+        
         repetitions = $('#repetitions').val()
+        $('#rounds').html("rounds remaining :"+repetitions)
         runclock();
     });
+    
 
     function runclock() {
-
+        if (repetitions > 0) {
+            $('audio#pop')[0].play()
+            }
         var duration = $('#duration').val();
         let dure=duration
+        
         repetitions--
         if (repetitions >= 0) {
-            $('#timer').html("time remaining is:"+dure)
+            $('#timer').html("time remaining in this round: "+dure+" seconds")
             let timerId = setInterval(() => {
                 dure--
-                $('#timer').html("time remaining is:"+dure)
+                $('#timer').html("time remaining in this round: "+dure+" seconds")
                 
             }, 1000);
 
@@ -28,6 +34,9 @@ $(document).ready(function () {
     }
 
     function setBreak() {
+        $('#rounds').html("rounds remaining :"+repetitions)
+        $('audio#pop')[0].play()
+        
         var breakTime = $('#break').val();
 
         console.log(repetitions)
